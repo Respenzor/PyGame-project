11eefe47667d30dfe4671b684b6d72180ecf781b
Respenzor <respenzor2020@gmail.com>
2024-01-09 16:50:56 +0700

    Пример меню игры (кнопеи не работают)

--- Пазлы.py
+++ Пазлы.py
@@ -1,5 +1,4 @@
 import pygame
-from utils import load_image
 pygame.init()
 
 W, H = 800, 800
@@ -13,8 +12,22 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    fon = pygame.transform.scale(load_image('fon.jpg'), (W, H))
+    intro_text = ["Играть",
+                  "Правила",
+                  "Настройки"]
+    fon = pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
     window.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 100)
+    text_coord = 250
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        window.blit(string_rendered, intro_rect)
+
     pygame.display.update()
     clock.tick(FPS)
 
