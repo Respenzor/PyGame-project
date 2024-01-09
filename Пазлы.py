35863946ae1184fa95c180950e4fdd64ea84a0a1
Respenzor <respenzor2020@gmail.com>
2024-01-09 16:35:25 +0700

    Каркас проекта

---
+++ Пазлы.py
@@ -0,0 +1,19 @@
+import pygame
+pygame.init()
+
+W, H = 800, 800
+FPS = 60
+
+window = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.update()
+    clock.tick(FPS)
+
+pygame.quit()
