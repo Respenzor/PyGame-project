875d3fa44bb93ae8041fb910eeb5d18ab854867c
Respenzor <respenzor2020@gmail.com>
2024-01-09 17:16:37 +0700

    Работа над меню игры. Необходимо добавить вызов функции start_screen для отображения меню игры, с отслеживанием нажатий кнопок мыши

--- Пазлы.py
+++ Пазлы.py
@@ -12,23 +12,31 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    intro_text = ["Играть",
-                  "Правила",
-                  "Настройки"]
-    fon = pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
-    window.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 100)
-    text_coord = 250
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        window.blit(string_rendered, intro_rect)
+    def start_screen(): #ФУНКЦИЯ НЕ ВЫЗВАНА
+        intro_text = ["Играть",
+                    "Правила",
+                    "Настройки"]
+        fon = pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
+        window.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 100)
+        text_coord = 250
 
-    pygame.display.update()
-    clock.tick(FPS)
+        for line in intro_text:
+            string_rendered = font.render(line, 1, pygame.Color('white'))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            window.blit(string_rendered, intro_rect)
 
-pygame.quit()
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return
+
+            pygame.display.update()
+            clock.tick(FPS)
