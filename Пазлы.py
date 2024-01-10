c7b89f5e43ba0c7257d30d7502d6565e141ab499
Respenzor <respenzor2020@gmail.com>
2024-01-10 16:11:36 +0700

    Переделана структура запуска, запуск меню работает

--- Пазлы.py
+++ Пазлы.py
@@ -6,37 +6,34 @@ FPS = 60
 window = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def start_screen():
+    intro_text = ["Играть",
+                "Правила",
+                "Настройки"]
+    fon = pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
+    window.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 100)
+    text_coord = 250
 
-    def start_screen(): #ФУНКЦИЯ НЕ ВЫЗВАНА
-        intro_text = ["Играть",
-                    "Правила",
-                    "Настройки"]
-        fon = pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
-        window.blit(fon, (0, 0))
-        font = pygame.font.Font(None, 100)
-        text_coord = 250
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        window.blit(string_rendered, intro_rect)
 
-        for line in intro_text:
-            string_rendered = font.render(line, 1, pygame.Color('white'))
-            intro_rect = string_rendered.get_rect()
-            text_coord += 10
-            intro_rect.top = text_coord
-            intro_rect.x = 10
-            text_coord += intro_rect.height
-            window.blit(string_rendered, intro_rect)
-
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                elif event.type == pygame.KEYDOWN or \
-                        event.type == pygame.MOUSEBUTTONDOWN:
-                    return
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return
 
             pygame.display.update()
             clock.tick(FPS)
+
+if __name__ == '__main__':
+    start_screen()
