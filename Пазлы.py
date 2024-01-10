1b7b095c9de94fd7da024763f4390c7589c34660
Respenzor <respenzor2020@gmail.com>
2024-01-10 18:18:30 +0700

    Отображение кнопок перенесено в класс Buttons. Для отображения кнопок нужно сделать его вызов в функции start_screen

--- Пазлы.py
+++ Пазлы.py
@@ -1,29 +1,20 @@
 import pygame
+import sys
 
 pygame.init()
 W, H = 800, 800
 FPS = 60
 window = pygame.display.set_mode((W, H))
 clock = pygame.time.Clock()
+UP = 'up'
+DOWN = 'down'
+LEFT = 'left'
+RIGHT = 'right'
 
 def start_screen():
-    intro_text = ["Играть",
-                "Правила",
-                "Настройки"]
     fon = pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
     window.blit(fon, (0, 0))
     font = pygame.font.Font(None, 100)
-    text_coord = 250
-
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        window.blit(string_rendered, intro_rect)
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -31,9 +22,42 @@ def start_screen():
             elif event.type == pygame.KEYDOWN or \
                     event.type == pygame.MOUSEBUTTONDOWN:
                 return
-
             pygame.display.update()
             clock.tick(FPS)
+class Buttons:
+    def __init__(self, x, y, width, height, text, image_location, image_actived_location=None, button_actived_location=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.image_location = pygame.image.load('knopka.png')
+        self.image_location = pygame.transform.scale(self.image_location, (width, height))
+        self.image_actived = self.image_location
+        if image_actived_location:
+            self.image_actived = pygame.image.load('knopka_active.png.png')
+            self.image_actived = pygame.transform.scale(self.image_actived, (width, height))
+        self.rect = self.image_location.get_rect(topleft=(x, y))
+        self.sound = None
+        if button_actived_location:
+            self.sound = pygame.mixer.Sound(button_actived_location)
+        self.actived = False
+    def draw(self, window):
+        proverka_knopki = self.image_actived if self.actived else self.image_location
+        window.blit(proverka_knopki, self.rect.topleft)
+        font = pygame.font.Font(None, 32)
+        text_surface = font.render(self.text, True, (225, 225, 225))
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        window.blit(text_surface, text_rect)
+
+    def check_active(self, mouse_pos):
+        self.actived = self.rect.collidepoint(mouse_pos)
+
+    def event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.actived:
+            if self.sound:
+                self.sound.play()
+            pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
 
 if __name__ == '__main__':
     start_screen()
