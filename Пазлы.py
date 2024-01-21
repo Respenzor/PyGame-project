68448c739b54dc366f074c9a343fe46aa89646f7
Respenzor <respenzor2020@gmail.com>
2024-01-21 22:47:24 +0700

    Добавлен класс Board для будущей основы окна игры. С помощью него можно сделать определение координат клетки, в которой произошло нажатие. Позже нужно сделать клетки частью пазла

--- Пазлы.py
+++ Пазлы.py
@@ -113,3 +113,68 @@ class Buttons:
 
 if __name__ == '__main__':
     start_screen()
+
+import pygame
+
+pygame.init()
+size = 500, 500
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('Координаты клетки')
+
+
+class Board:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.board = [[1] * width for _ in range(height)]
+        self.left = 10
+        self.top = 10
+        self.cell_size = 30
+
+    def render(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
+                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size, self.cell_size),
+                                 self.board[y][x])
+
+    def set_view(self, left, top, cell_size):
+        self.left = left
+        self.top = top
+        self.cell_size = cell_size
+
+    def on_click(self, cell_coords):
+        print(cell_coords)
+
+    def get_cell(self, mouse_pos):
+        if self.left <= mouse_pos[1] < self.left + self.height * self.cell_size and self.top <= mouse_pos[0] < self.top + self.width * self.cell_size:
+            return (int((mouse_pos[1] - self.left) / self.cell_size), int((mouse_pos[0] - self.top) / self.cell_size))
+        else:
+            return None
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell != None:
+            self.on_click(cell)
+        if cell == None:
+            self.on_click(cell)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = 400, 450
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Инициализация игры')
+    board = Board(5, 7)
+    board.set_view(50, 50, 50)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            board.get_click(event.pos)
+    screen.fill((0, 0, 0))
+    board.render()
+    pygame.display.flip()
+pygame.quit()
