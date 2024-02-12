1547f25b494c9a305c189159b0008d9d3dafc6bc
Respenzor <151031951+Respenzor@users.noreply.github.com>
2024-02-12 20:33:38 +0700

    Размер окна изменён на 1920 к 1080. Добавлено отображение картинки и создание её спрайтов. Необходимо исправить ошибку с перекрытием картинкой клетчатого поля

--- Пазлы.py
+++ Пазлы.py
@@ -1,23 +1,24 @@
 import pygame
 import sys
-import random
-from random import randint
-
-pygame.init()
-W, H = 800, 700
-FPS = 60
-window = pygame.display.set_mode((W, H))
-clock = pygame.time.Clock()
 
 UP = 'up'
 DOWN = 'down'
 LEFT = 'left'
 RIGHT = 'right'
-
 GAME_NAME = 'Magick Puzzles'
 BUTTON_IMAGE = 'knopka.png'
 BUTTON_ACTIVE_IMAGE = 'knopka_active.png'
 BUTTON_SOUND = 'knopka_press.mp3'
+IMAGE = pygame.image.load('level 1.jpeg')
+WAIT = 'Загрузка...'
+
+pygame.init()
+W_game, H_game = IMAGE.get_width(), IMAGE.get_height()
+pygame.display.set_caption(GAME_NAME)
+W, H = 1920, 1080
+FPS = 60
+window = pygame.display.set_mode((W, H))
+clock = pygame.time.Clock()
 
 
 def main():
@@ -49,9 +50,10 @@ def start_screen(window, clock, FPS, W, H, GAME_NAME, BUTTON_IMAGE, BUTTON_ACTIV
     window.blit(game_name, game_name_rect)
     pygame.display.flip()
 
-    start_button = Buttons(250, 225, 300, 100, 'Играть', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
-    options_button = Buttons(250, 355, 300, 100, 'Настройки', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
-    quit_button = Buttons(250, 475, 300, 100, 'Выход', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+    start_button = Buttons(W // 2 - 250, 225, 500, 200, 'Играть', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+    options_button = Buttons(W // 2 - 250, 450, 500, 200, 'Настройки', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+    quit_button = Buttons(W // 2 - 250, 675, 500, 200, 'Выход', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+
     buttons = [start_button, options_button, quit_button]
     show_menu = True
 
@@ -67,10 +69,11 @@ def start_screen(window, clock, FPS, W, H, GAME_NAME, BUTTON_IMAGE, BUTTON_ACTIV
             elif event.type == pygame.USEREVENT:
                 if show_menu:
                     if event.button == start_button:
-                        pygame.display.set_mode((1000, 800))
+                        pygame.display.set_mode((W_game, H_game))
                         window.fill((0, 0, 0))
-                        board = Board(W, H)
-                        board.render()
+                        board = Board(W_game, H_game, IMAGE)
+                        board.set_view(0, 0, min(W // board.width, H // board.height))
+                        board.create(window)
                         show_menu = False
                     elif event.button == options_button:
                         print('Меню настроек')
@@ -87,7 +90,10 @@ def start_screen(window, clock, FPS, W, H, GAME_NAME, BUTTON_IMAGE, BUTTON_ACTIV
             for button in buttons:
                 button.draw(window)
         else:
-            board.render()
+            board.create(window)  # Загрузка доски
+            board.render(window) #Отрисовка доски
+            pygame.display.flip()
+            pygame.display.update()
             return
 
         pygame.display.update()
@@ -116,7 +122,7 @@ class Buttons:
     def draw(self, window):
         proverka_knopki = self.image_actived if self.actived else self.image_location
         window.blit(proverka_knopki, self.rect.topleft)
-        font = pygame.font.Font(None, 32)
+        font = pygame.font.Font(None, 60)
         text_surface = font.render(self.text, True, (225, 225, 225))
         text_rect = text_surface.get_rect(center=self.rect.center)
         window.blit(text_surface, text_rect)
@@ -132,24 +138,50 @@ class Buttons:
 
 
 class Board:
-    def __init__(self, width, height):
+    def __init__(self, width, height, image):
         self.width = width
         self.height = height
         self.board = [[1] * width for _ in range(height)]
         self.left = 0  # смещение по оси абцисс
         self.top = 0  # смещение по оси ординат
-        self.cell_size = 100  # размер клеток
+        self.cell_size = 120
+        self.image = image
+        self.loaded = False
+        self.sprite_images = []
 
-    def render(self):
-        window.fill((0, 0, 0))
-        for y in range(self.height):
-            for x in range(self.width):
-                cell_left = x * self.cell_size + self.left
-                cell_top = y * self.cell_size + self.top
-                pygame.draw.rect(window, pygame.Color(255, 255, 255),
-                                 (cell_left, cell_top, self.cell_size, self.cell_size), 1)
-        pygame.display.flip()
-
+    def create(self, window):
+        if not self.loaded:
+            window.fill((0, 0, 0))
+            font = pygame.font.Font(None, 125)
+            wait = font.render(WAIT, True, (255, 255, 255))
+            wait_rect = wait.get_rect(center=(W_game // 2, H_game // 2))
+            window.blit(wait, wait_rect)
+            pygame.display.flip()
+            sprite_width = self.image.get_width() // self.width
+            sprite_height = self.image.get_height() // self.height
+            for y in range(self.height):
+                sprite_row = []
+                for x in range(self.width):
+                    sprite_rect = pygame.Rect(x * sprite_width, y * sprite_height, sprite_width, sprite_height)
+                    sprite_image = pygame.Surface((sprite_width, sprite_height))
+                    sprite_image.blit(self.image, (0, 0), area=sprite_rect)
+                    sprite_row.append(sprite_image)
+                self.sprite_images.append(sprite_row)
+            pygame.display.flip()
+            self.loaded = True
+    def render(self, window):
+        if self.loaded:
+            window.fill((0, 0, 0))
+            for y in range(self.height):
+                for x in range(self.width):
+                    cell_left = x * self.cell_size + self.left
+                    cell_top = y * self.cell_size + self.top
+                    cell_rect = pygame.Rect(cell_left, cell_top, self.cell_size, self.cell_size)
+                    pygame.draw.rect(window, pygame.Color(255, 255, 255), cell_rect, 1)
+                    cell_image = self.sprite_images[y][x]
+                    window.blit(cell_image, (cell_left, cell_top))
+            pygame.display.flip()
+            pygame.display.update()
 
     def set_view(self, left, top, cell_size):
         window_width, window_height = pygame.display.get_surface().get_size()
@@ -159,6 +191,8 @@ class Board:
         top = (window_height - board_height) // 2
         self.left = left
         self.top = top
+        self.cell_size_x = cell_size
+        self.cell_size_y = cell_size * self.height / self.width
         self.cell_size = cell_size
 
     def on_click(self, cell_coords):
@@ -172,5 +206,5 @@ class Board:
 
 
 if __name__ == '__main__':
-    board = Board(W, H)
+    board = Board(W_game, H_game, IMAGE)
     main()
