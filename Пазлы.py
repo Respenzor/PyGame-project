27b7742469274641c7c885b853ae422ee399d2a4
Tualetniy Posral <xxionnixxosx17@gmail.com>
2024-02-25 15:33:21 +0700

    я добавил чтобы кнопк 747yy

--- Пазлы.py
+++ Пазлы.py
@@ -47,14 +47,14 @@ def start_screen():
                     print('0 ')
                 elif event.button == options_button:
                     pygame.display.set_mode((800, 700))
-                    start_button = Buttons(250, 225, 300, 100, 'Настройки музыки и звуков', BUTTON_IMAGE,
-                                           BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
-                    options_button = Buttons(250, 355, 300, 100, 'Настройки управления', BUTTON_IMAGE,
-                                             BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
-                    quit_button = Buttons(250, 475, 300, 100, '____', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
-                    buttons = [start_button, options_button, quit_button]
                     pygame.transform.scale(pygame.image.load('fon_menu.jpg'), (W, H))
                     pygame.display.flip()
+                    start_button = Settings(250, 225, 300, 100, 'Настройки музыки и звуков', BUTTON_IMAGE,
+                                           BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+                    options_button = Settings(250, 355, 300, 100, 'Настройки управления', BUTTON_IMAGE,
+                                             BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+                    quit_button = Settings(250, 475, 300, 100, '____', BUTTON_IMAGE, BUTTON_ACTIVE_IMAGE, BUTTON_SOUND)
+                    buttons = [start_button, options_button, quit_button]
                 elif event.button == quit_button:
                     pygame.quit()
                     sys.exit()
@@ -103,6 +103,45 @@ class Buttons:
             if self.sound:
                 self.sound.play()
             pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
+
+
+class Settings:
+    def __init__(self, x, y, width, height, text, image_location, image_actived_location=None,
+                 button_actived_location=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.text = text
+        self.image_location = pygame.image.load('knopka.png')
+        self.image_location = pygame.transform.scale(self.image_location, (width, height))
+        self.image_actived = self.image_location
+        if image_actived_location:
+            self.image_actived = pygame.image.load('knopka_active.png')
+            self.image_actived = pygame.transform.scale(self.image_actived, (width, height))
+        self.rect = self.image_location.get_rect(topleft=(x, y))
+        self.sound = None
+        if button_actived_location:
+            self.sound = pygame.mixer.Sound(button_actived_location)
+        self.actived = False
+
+    def draw(self, window):
+        proverka_knopki = self.image_actived if self.actived else self.image_location
+        window.blit(proverka_knopki, self.rect.topleft)
+        font = pygame.font.Font(None, 32)
+        text_surface = font.render(self.text, True, (225, 225, 225))
+        text_rect = text_surface.get_rect(center=self.rect.center)
+        window.blit(text_surface, text_rect)
+
+    def check_active(self, mouse_pos):
+        self.actived = self.rect.collidepoint(mouse_pos)
+
+    def event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 and self.actived:
+            if self.sound:
+                self.sound.play()
+            pygame.event.post(pygame.event.Event(pygame.USEREVENT, button=self))
+
 
 if __name__ == '__main__':
     start_screen()
